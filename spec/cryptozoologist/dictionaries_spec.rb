be5773bf671dd29d3096c6ed3b11e1ef1fdd865c
Liz Abinante <me@liz.codes>
2016-04-11 20:21:27 -0700

    :123: fix typo-d method names, reading the docs helps

--- spec/cryptozoologist/dictionaries_spec.rb
+++ spec/cryptozoologist/dictionaries_spec.rb
@@ -3,17 +3,17 @@ require_relative '../spec_helper'
 describe Cryptozoologist::Dictionaries do
   context '#words' do
     it 'has a word list' do
-      expect(Cryptozoologist::Dictionaries.words.length).to_be > 1
+      expect(Cryptozoologist::Dictionaries.words.length).to be > 1
     end
 
     it 'contains Common animals' do
       common = Cryptozoologist::Dictionaries::Common::LIST
-      expect(Cryptozoologist::Dictionaries.words.includes?(common.sample)).to be_true
+      expect(Cryptozoologist::Dictionaries.words.include?(common.sample)).to be true
     end
 
     it 'contains Mythical creatures' do
       mythical = Cryptozoologist::Dictionaries::Mythical::LIST
-      expect(Cryptozoologist::Dictionaries.words.includes?(mythical.sample)).to be_true
+      expect(Cryptozoologist::Dictionaries.words.include?(mythical.sample)).to be true
     end
   end
 
--- spec/cryptozoologist/dictionary_spec.rb
+++ spec/cryptozoologist/dictionary_spec.rb
@@ -6,16 +6,16 @@ describe Cryptozoologist::Dictionary do
   end
 
   it 'has a word list' do
-    expect(@dictionary.words.length).to_be > 1
+    expect(@dictionary.words.length).to be > 1
   end
 
   it 'contains Common animals' do
     common = Cryptozoologist::Dictionaries::Common::LIST
-    expect(@dictionary.words.include?(common.sample)).to be_true
+    expect(@dictionary.words.include?(common.sample)).to be true
   end
 
   it 'contains Mythical creatures' do
     mythical = Cryptozoologist::Dictionaries::Mythical::LIST
-    expect(@dictionary.words.include?(mythical.sample)).to be_true
+    expect(@dictionary.words.include?(mythical.sample)).to be true
   end
 end
