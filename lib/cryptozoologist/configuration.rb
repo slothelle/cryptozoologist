30b9f9789293ee3a23adf46eb445119214a2993b
Liz Abinante <me@liz.codes>
2016-05-23 00:08:12 -0400

    :tophat: order + delimeter configs with defaults

--- lib/cryptozoologist/configuration.rb
+++ lib/cryptozoologist/configuration.rb
@@ -1,9 +1,25 @@
 module Cryptozoologist
   class Configuration
-    attr_reader :exclude
+    attr_reader :exclude, :delimeter, :order
 
     def initialize
       @exclude = []
+      @delimeter = "-"
+      @order = [:colors, :animals, :clothing]
+    end
+
+    def order=(list)
+      raise Errors::Configuration, "Order must be an array" unless list.is_a?(Array)
+      raise Errors::Configuration, "Order must contain 3 items" unless list.length == 3
+      raise Errors::Configuration, "Invalid order setting provided" if list.any? { |e| !valid_order.include?(e) }
+
+      @order = list
+    end
+
+    def delimeter=(string)
+      raise Errors::Configuration, "Delimeter must be a a string" unless string.is_a?(String)
+
+      @delimeter = string
     end
 
     def exclude=(exclusions)
@@ -13,6 +29,10 @@ module Cryptozoologist
     end
 
     private
+    def valid_order
+      [:animals, :colors, :clothing]
+    end
+
     def valid_exclusions
       keys = []
       Cryptozoologist.subdictionaries.each { |key, value| keys += value.keys }
--- spec/cryptozoologist/configuration_spec.rb
+++ spec/cryptozoologist/configuration_spec.rb
@@ -25,4 +25,71 @@ describe Cryptozoologist::Configuration do
       }.to raise_error(Cryptozoologist::Errors::Configuration)
     end
   end
+
+  context '#delimeter' do
+    it 'defaults to "-"' do
+      expect(Cryptozoologist.generate.match('-')).to be_instance_of(MatchData)
+    end
+
+    it 'requires a string' do
+      expect {
+        Cryptozoologist.configure do |config|
+          config.delimeter = 1
+        end
+      }.to raise_error(Cryptozoologist::Errors::Configuration)
+    end
+
+    it 'changes the delimeter when provided' do
+      Cryptozoologist.configure do |config|
+        config.delimeter ='$'
+      end
+
+      expect(Cryptozoologist.generate.match('$')).to be_instance_of(MatchData)
+    end
+  end
+
+  context '#order' do
+    it 'defaults to colors, animals, clothing' do
+      expect(Cryptozoologist.configuration.order).to eq([:colors, :animals, :clothing])
+    end
+
+    it 'rejects everything if anything invalid is passed' do
+      expect {
+        Cryptozoologist.configure do |config|
+          config.order = [:colors, :animals, :tacos]
+        end
+      }.to raise_error(Cryptozoologist::Errors::Configuration)
+    end
+
+    it 'rejects everything if fewer than 3 keys are passed' do
+      expect {
+        Cryptozoologist.configure do |config|
+          config.order = [:colors, :animals]
+        end
+      }.to raise_error(Cryptozoologist::Errors::Configuration)
+    end
+
+    it 'rejects everything if more than 3 keys are passed' do
+      expect {
+        Cryptozoologist.configure do |config|
+          config.order = [:colors, :animals, :clothing, :tacos]
+        end
+      }.to raise_error(Cryptozoologist::Errors::Configuration)
+    end
+
+    it 'requires an array' do
+      expect {
+        Cryptozoologist.configure do |config|
+          config.order = 1
+        end
+      }.to raise_error(Cryptozoologist::Errors::Configuration)
+    end
+
+    it 'changes the order of keys' do
+      Cryptozoologist.configure do |config|
+        config.order = [:clothing, :colors, :animals]
+      end
+      expect(Cryptozoologist.configuration.order).to eq([:clothing, :colors, :animals])
+    end
+  end
 end
